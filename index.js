eccf1054dd6e91a7587406a7b57fb237a1218ed0
elimhuebner@gmail.com <ehbner@DESKTOP-O6P2HM5.localdomain>
2020-12-17 17:38:40 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,29 @@
 // Add your functions here
+const map = function(arr, fnc){
+    let newArr = []
+    for(const element of arr){
+        newArr.push(fnc(element))
+    }
+    return newArr
+}
+
+const reduce = function(arr, fnc, start){
+    // let total = start
+    // arr.forEach(element => {
+    //     total = fnc(total, element)
+    // })
+    // return total
+    let i = 1
+    let total
+    if(start){
+        i = 0
+        total = start
+    } else {
+        total = arr[0]
+    }
+
+    for (i; i < arr.length; i++){
+        total = fnc(total, arr[i])
+    }
+    return total
+}
